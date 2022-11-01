4fc1760a3b46ed907c32fcf3f8f9aef6d884836e
Jotirmay manik <jotirmaymanik@gmail.com>
2022-11-01 15:07:36 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+          <div>
+        <h1>TechSter</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://instagram.com" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default footer
